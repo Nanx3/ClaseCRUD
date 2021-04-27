f78f8734e3d72ab6efad5f176d11ba8b2ac6c208
Nancy Espinosa <nancyespinosax3@gmail.com>
2021-04-27 19:23:58 +0200

    tasks

--- controllers/task.php
+++ controllers/task.php
@@ -3,81 +3,77 @@
 require_once 'db.php';
 
 class Task {
-    
     function list() {
-      $connection = new Connection;
-      
-      // Crear la conexión
-      $mysql = $connection->create();
-      
-      $result = $mysql->query("SELECT * FROM tasks");
-      $tasks = $result->fetch_all(MYSQLI_ASSOC);
-      
-      // Cerrar conexión
-      $connection->close($mysql);
-      
-      return $tasks;
+        $connection = new Connection();
+        
+        //Crear la conexión
+        $mysql = $connection->create();
+        
+        $sql = "SELECT * FROM tasks";
+        $result = $mysql->query($sql);
+        $tasks = $result->fetch_all(MYSQLI_ASSOC);
+        
+        // Cerrar conexión
+        $connection->close($mysql);
+        
+        return $tasks;        
     }
     
     function create($request) {
-      $connection = new Connection;
-      
-      // Crear la conexión
-      $mysql = $connection->create();
-      
-      $result = $mysql->query("INSERT INTO tasks(description,status) values ('{$request['description']}','{$request['status']}')");
-      
-      // Cerrar conexión
-      $connection->close($mysql);
-      
-      return $result;  
+        $connection = new Connection();
+        
+        //Crear la conexión
+        $mysql = $connection->create();
+        
+        $sql = "INSERT INTO tasks(description,status) values('{$request['description']}', '{$request['status']}')";
+        $result = $mysql->query($sql);
+        
+        $connection->close($mysql);
+        
+        return $result;
     }
     
     function show($id) {
-      $connection = new Connection;
-      
-      // Crear la conexión
-      $mysql = $connection->create();
-      
-      $result = $mysql->query("SELECT * FROM tasks WHERE id = $id");
-      $task = $result->fetch_assoc();
-      
-      // Cerrar conexión
-      $connection->close($mysql);
-      
-      return $task;
+        $connection = new Connection();
         
+        //Crear la conexión
+        $mysql = $connection->create();
         
+        $sql = "SELECT * FROM tasks WHERE id = $id";
+        $result = $mysql->query($sql);
+     
+        $task = $result->fetch_assoc();
+        
+        $connection->close($mysql);
+        
+        return $task;        
     }
     
     function update($request) {
-      $connection = new Connection;
-      
-      // Crear la conexión
-      $mysql = $connection->create();
-      
-      $result = $mysql->query("UPDATE tasks SET description = '{$request['description']}', status = '{$request['status']}' WHERE id = {$request['id']}");
-      
-      // Cerrar conexión
-      $connection->close($mysql);
-      
-      return $result;
+        $connection = new Connection();
         
+        //Crear la conexión
+        $mysql = $connection->create();
+        
+        $sql = "UPDATE tasks SET description = '{$request['description']}', status='{$request['status']}' WHERE id = '{$request['id']}'";
+        $result = $mysql->query($sql);
+             
+        $connection->close($mysql);
+        
+        return $result;          
     }
     
     function delete($id) {
-      $connection = new Connection;
-      
-      // Crear la conexión
-      $mysql = $connection->create();
-      
-      $result = $mysql->query("DELETE FROM tasks WHERE id = $id");
-      
-      // Cerrar conexión
-      $connection->close($mysql);
-      
-      return $result;
+        $connection = new Connection();
         
+        //Crear la conexión
+        $mysql = $connection->create();
         
+        $sql = "DELETE FROM tasks WHERE id = $id";
+        $result = $mysql->query($sql);
+             
+        $connection->close($mysql);
+        
+        return $result;     
     }
 }
